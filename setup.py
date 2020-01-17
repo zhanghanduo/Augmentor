437531e02b70db64b994dfe66217f31f813ab722
Marcus Bloice <marcus.bloice@medunigraz.at>
2020-01-17 12:55:25 +0100

    Update to 0.2.8. Fixes issue #209

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Augmentor',
     packages=['Augmentor'],
-    version='0.2.7',
+    version='0.2.8',
     description='Image augmentation library for Machine Learning',
     long_description='Image augmentation library for Machine Learning',
     license='MIT',
